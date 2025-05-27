6854ee43907472b8edbbcade99cc674ab7c077fe
3-stardust-7 <vkirubha7@gmail.com>
2025-05-27 16:02:35 +0530

    Implement basic CRUD endpoints using FastAPI and Pydantic models

--- main.py
+++ main.py
@@ -23,13 +23,22 @@ def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items",response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
     if 0 <= item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, updated_item: Item):
+    items[item_id] = updated_item
+    return updated_item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return items.pop(item_id)
